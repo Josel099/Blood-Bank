fb54ad578ae3aba922d1a7915c5956e1e1056e6b
Josel099 <joseltech2002@gmail.com>
2025-01-18 16:53:52 +0530

    feat: create new entities.

---
+++ src/main/java/com/freelancenexus/bloodbank/db/entities/BloodInfo.java
@@ -0,0 +1,10 @@
+package com.freelancenexus.bloodbank.db.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "blood_info")
+public class BloodInfo extends BaseEntity {
+
+}
---
+++ src/main/java/com/freelancenexus/bloodbank/db/entities/BloodRequestInfo.java
@@ -0,0 +1,10 @@
+package com.freelancenexus.bloodbank.db.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "blood_request_info")
+public class BloodRequestInfo extends BaseEntity {
+
+}
