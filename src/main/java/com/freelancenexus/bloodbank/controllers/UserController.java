72d3755f0918c69dcfb68e9adaeee5e0cdd3e8fa
Josel099 <joseltech2002@gmail.com>
2025-01-22 00:08:24 +0530

    swagger updation v1.

--- src/main/java/com/freelancenexus/bloodbank/controllers/UserController.java
+++ src/main/java/com/freelancenexus/bloodbank/controllers/UserController.java
@@ -27,10 +27,12 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1/user")
+@Tag(name = "User related Operation", description = "APIs for managing users")
 public class UserController {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
